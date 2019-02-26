2e25ff1694f3511cb445fc8172e5b4a754fdb905
Plota16 <michalplocki2@gmail.com>
2019-02-26 11:26:16 +0100

    Class separation

--- PiCarServer.py
+++ PiCarServer.py
@@ -1,9 +1,54 @@
 from bluetooth import *
+from enum import Enum
+import time
+
+class Movment(Enum):
+    forward = "Going forward"
+    stop = "Stopping"
+    backward = "Going backward"
+
+
+class Turning(Enum):
+    left = "Turning left"
+    right = "Turning right"
+    straight = "Going straight"
+
+
+class Lights(Enum):
+    on = "on"
+    off = "off"
+
+
+class Car:
+    def __init__(self):
+        self.movement = Movment.stop
+        self.turning = Turning.straight
+        self.lights = Lights.off
+
+    def turn_right(self):
+        self.turning = Turning.right
+
+    def turn_left(self):
+        self.turning = Turning.left
+
+    def go_straight(self):
+        self.turning = Turning.straight
+
+    def go_forward(self):
+        self.movement = Movment.forward
+
+    def go_backward(self):
+        self.movement = Movment.backward
+
+    def stop(self):
+        self.movement = Movment.stop
+
 
 
 server_sock = BluetoothSocket(RFCOMM)
 server_sock.bind(("", PORT_ANY))
 server_sock.listen(1)
+
 
 port = server_sock.getsockname()[1]
 
@@ -15,26 +60,39 @@ advertise_service(server_sock, "AquaPiServer",
                   profiles=[SERIAL_PORT_PROFILE],
                   #                   protocols = [ OBEX_UUID ]
                   )
-print("Waiting for connection on RFCOMM channel %d" % port)
+print("waiting for connection on RFCOMM channel %d" % port)
 client_sock, client_info = server_sock.accept()
 print("Accepted connection from ", client_info)
+client_sock.setblocking(0)
+car = Car()
 
 while True:
+
+    print(car.movement.value + " " + car.turning.value)
+    time.sleep(0.1)
+
     try:
         data = client_sock.recv(1024)
         if len(data) == 0: break
         #print("received [%s]" % data)
-             
-        if data == b'up':
-            print("Go forward")
-        elif data == b'down':
-            print("Go backward")
+
+        if data == b'go':
+            car.go_forward()
+        elif data == b'go_back':
+            car.go_backward()
+        elif data == b'stop':
+            car.stop()
         elif data == b'left':
-            print("Turn left")
+            car.turn_left()
         elif data == b'right':
-            print("Turn right")
+            car.turn_right()
+        elif data == b'straight':
+            car.go_straight()
         else:
             data = 'WTF!'
+            print("WTF")
+
+
 
         #client_sock.send(data)
         #print("sending [%s]" % data)
@@ -49,5 +107,4 @@ while True:
         client_sock.close()
         server_sock.close()
         print("all done")
-
         break
